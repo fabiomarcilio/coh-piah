05ee360b91169bbf6fcf207c4bba2dc32c98c77e
Fabio Marcilio <fabiomarcilio@msn.com>
2020-08-10 10:37:04 -0300

    Programa COH-PIAH finalizado!

--- coh-piah.py
+++ coh-piah.py
@@ -4,14 +4,14 @@ def le_assinatura():
     '''A funcao le os valores dos tracos linguisticos do modelo e devolve uma assinatura a ser comparada com os textos fornecidos'''
     print("Bem-vindo ao detector automático de COH-PIAH.")
     print("Informe a assinatura típica de um aluno infectado:")
-
+    print()
     wal = float(input("Entre o tamanho médio de palavra:"))
     ttr = float(input("Entre a relação Type-Token:"))
     hlr = float(input("Entre a Razão Hapax Legomana:"))
     sal = float(input("Entre o tamanho médio de sentença:"))
     sac = float(input("Entre a complexidade média da sentença:"))
     pal = float(input("Entre o tamanho medio de frase:"))
-
+    print()
     return [wal, ttr, hlr, sal, sac, pal]
 
 def le_textos():
@@ -22,8 +22,8 @@ def le_textos():
     while texto:
         textos.append(texto)
         i += 1
+        print()
         texto = input("Digite o texto " + str(i) +" (aperte enter para sair):")
-
     return textos
 
 def separa_sentencas(texto):
@@ -74,9 +74,10 @@ def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
     i = 0
     soma = 0
-    for objeto in as_a:
+    for assinatura in as_a:
         soma = soma + (abs(as_a[i] - as_b[i]))
         i = i + 1
+    return soma / 6
     
 
 def calcula_assinatura(texto):
@@ -109,8 +110,25 @@ def calcula_assinatura(texto):
     sac = len(lista_frases) / len(lista_sentencas)  # sac - Complexidade média da sentença
     pal = somaFrase / len(lista_frases)  # pal - Tamanho medio de frase
 
-    return print([wal, ttr, hlr, sal, sac, pal])
+    return [wal, ttr, hlr, sal, sac, pal]
 
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    resultado = []
+    for tex in textos:
+        ass_text = []
+        ass_text = calcula_assinatura(tex)
+        resultado.append(compara_assinatura(ass_text, ass_cp))
+    
+    valor_min = min(resultado)
+    posicao = resultado.index(valor_min) + 1
+  
+    return posicao
+
+assinatura_infectado = []
+textos = []
+assinatura_infectado = le_assinatura()
+textos = le_textos()
+resultado = avalia_textos(textos,assinatura_infectado)
+print()
+print('O autor do texto', resultado ,'está infectado com COH-PIAH')
